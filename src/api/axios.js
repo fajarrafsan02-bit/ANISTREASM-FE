c788a410b635c33f1592fb4e4893dadb2337d0e2
fajarrafsan02-bit <fajar.rafsan02@gmail.com>
2026-06-16 23:39:41 +0700

    initial commit last

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API,
+    baseURL: 'http://localhost:3000/api',
     timeout: 30000,
     withCredentials: true
 });
